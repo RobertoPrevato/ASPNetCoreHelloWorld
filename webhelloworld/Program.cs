662385e7d1bcaf8a9919a62e691c8c7e8f6ad49a
Roberto Prevato <roberto.prevato@gmail.com>
2017-10-19 21:13:28 +0200

    first commit

---
+++ webhelloworld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace webhelloworld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) {
+            var serverPort = Environment.GetEnvironmentVariable("SERVER_PORT") ?? "5000";
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls($"http://0.0.0.0:{serverPort}")
+                .Build();
+        }
+    }
+}
